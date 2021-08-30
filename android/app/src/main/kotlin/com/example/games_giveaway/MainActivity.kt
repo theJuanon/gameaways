a091354fbbd28e4a96741f4d6f3a0d5b88304e7d
theJuanon <jhernandezangulo@gmail.com>
2021-08-29 20:49:09 -0600

    init

---
+++ android/app/src/main/kotlin/com/example/games_giveaway/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.games_giveaway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/game.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
